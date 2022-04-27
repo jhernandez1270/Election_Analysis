3fdda5bfdde27bf46268f0ab8411e1522f979820
Jesse Hernandez <jessehernandez@Jesses-Home-MBPro.local>
2022-04-26 18:21:46 -0700

    updating PyPoll_Challenge.py for Deliverable 2 and saving results in election_results.txt

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -8,7 +8,7 @@ import os
 # Add a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_results.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
@@ -114,6 +114,7 @@ with open(file_to_save, "w") as txt_file:
         print(county_results)
 
          # 6e: Save the county votes to a text file.
+        txt_file.write(county_results)
 
          # 6f: Write an if statement to determine the winning county and get its vote count.
         if c_votes > largest_county_votes:
@@ -131,6 +132,7 @@ with open(file_to_save, "w") as txt_file:
 
 
     # 8: Save the county with the largest turnout to a text file.
+    txt_file.write(largest_turnout)
 
 
     # Save the final candidate vote count to the text file.
